b28ba5ddf7f16bb048ea215e5a6382e603fa6c93
shermondevelopment <victorshermon@yahoo.com>
2023-03-20 20:26:54 -0300

    exercise resolution

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Tabela de multiplicação");
+
+        int number = 2;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + number * i);
+        }
+    }
+}
